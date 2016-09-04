9a38f1a30a277d6bbcd7114fb7dc4224f7dc354b
Stephen Ma <tehphen@gmail.com>
2016-09-04 16:15:41 -0700

    Make build work.

---
+++ convert.cpp
@@ -0,0 +1,5 @@
+// Aseprite Convert to SFML Library
+// Copyright (c) 2016 Bablawn3d5 - <stephen.ma@bablawn.com>
+//
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
